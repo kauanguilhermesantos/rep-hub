8db9f34ec89b0c057bfe167bc09443b44c590b14
Kauan Santos <kauansantos1411@outlook.com>
2025-04-15 10:57:25 -0300

    feat: melhorando header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 
 import Header from "@/components/Header";
-import Main from "@/components/Main";
+// import Main from "@/components/Main";
+
+import Image from "next/image";
+
 
 export default function LandingPage() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header/>
       <main>
-        <Main/>
+        
       </main>
     </div>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@/components/ui/button"
+import Image from "next/image"
+
+import logo from "@/img/logo-rephub.png"
 
 export default function Header() {
     return (
       <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="flex h-16 items-center justify-between px-22 py-10">
-          <span>RepHub</span>
+          <div className="flex gap-1 items-center">
+            <Image src={logo} alt={"Logo Rep Hub"} className="w-40"/>
+          </div>
           <div className="flex items-center gap-2 py-8">
             <Button variant={"ghost"}>Entrar</Button>
             <Button>Registrar</Button>
