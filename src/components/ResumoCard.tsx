505a352517fba9728db227fc23110a0e1e6ea9a1
Kauan Santos <kauansantos1411@outlook.com>
2026-09-02 00:12:13 -0300

    fix(home): resumoCard

--- src/components/ResumoCard.tsx
+++ src/components/ResumoCard.tsx
@@ -1,15 +1,13 @@
 export default function ResumoCard({ title, value, icon: Icon, color }: any) {
   return (
-    <div className="bg-white rounded-xl shadow-sm p-4 border border-gray-100 hover:shadow-md transition-shadow">
-        <div className="flex items-center justify-around ">
-            <div className={`p-3 rounded-lg bg-${color}-50`}>
-                <Icon className={`text-${color}-600`} size={26} />
-            </div>
-            <div>
-                <h4 className="text-sm font-medium text-gray-500 mb-1">{title}</h4>
-                <p className="text-2xl font-bold text-gray-800">{value}</p>
-            </div>
+    <div className="bg-white rounded-xl shadow-sm p-6 border border-gray-100">
+      <div className="flex items-center justify-between mb-3">
+        <div className={`p-3 rounded-lg bg-${color}-50`}>
+          <Icon className={`text-${color}-600`} size={24} />
         </div>
+      </div>
+      <p className="text-sm text-gray-500 mb-1">{title}</p>
+      <p className="text-2xl font-bold text-gray-800">{value}</p>
     </div>
   )
 }
